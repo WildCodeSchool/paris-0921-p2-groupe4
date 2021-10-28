5580179d7f12ed861eabceb5b710d77218f3ee44
elisacruzcas <elisa.castrillo.cruz@gmail.com>
2021-10-28 13:58:15 +0200

    Homepage works with all links

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Happy from '../Pictures/Happy-news.jpg';
 import National from '../Pictures/Monkey-2.jpg';
 import Tendances from '../Pictures/Tendances.jpg';
@@ -9,48 +8,36 @@ import './Text.css';
 
 export default function Pictures() {
   return (
-    <div className="Container-pictures">
-      <div>
-        <div className="WelcomeText">
-          <p> Welcome to your relaxation App. Choose below what you want to see </p>
+    <>
+      <div className="WelcomeText">
+        <p> Welcome to your relaxation App.Choose below what you want to see </p>
+      </div>
+      <div className="Container-pictures">
+        <div className="ContainerOne">
+          <div className="Pictures">
+            <a href="/Happynews">
+              <img className="Happy" src={Happy} alt="Happy-news" height="150px"></img>
+            </a>
+          </div>
+          <div className="Pictures">
+            <a href="/National">
+              <img className="National" src={National} alt="National-geographic" height="170px"></img>
+            </a>
+          </div>
+        </div>
+        <div className="ContainerTwo">
+          <div className="Pictures">
+            <a href="/Tendances">
+              <img className="Tendances" src={Tendances} alt="Tendances" height="170px"></img>
+            </a>
+          </div>
+          <div className="Pictures">
+            <a href="/TED">
+              <img className="TED" src={TED} alt="TED" height="170px"></img>
+            </a>
+          </div>
         </div>
       </div>
-      <div className="Pictures">
-        <img className="Happy" src={Happy} alt="Happy-news" height="150px"></img>
-        <ul>
-          <li>
-            {' '}
-            <Link to="/HappyNews"> Happy News </Link>{' '}
-          </li>
-        </ul>
-      </div>
-      <div className="Pictures">
-        <img className="National" src={National} alt="National-geographic" height="170px"></img>
-        <ul>
-          <li>
-            {' '}
-            <Link to="/National"> National Geographic </Link>{' '}
-          </li>
-        </ul>
-      </div>
-      <div className="Pictures">
-        <img className="Tendances" src={Tendances} alt="Tendances" height="170px"></img>
-        <ul>
-          <li>
-            {' '}
-            <Link to="/Tendances"> Tendances </Link>{' '}
-          </li>
-        </ul>
-      </div>
-      <div className="Pictures">
-        <img className="TED" src={TED} alt="TED" height="170px"></img>
-        <ul>
-          <li>
-            {' '}
-            <Link to="/TedTalks"> TED Talks </Link>{' '}
-          </li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 }
