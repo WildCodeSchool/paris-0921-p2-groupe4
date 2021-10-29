42a92081cb795d6ed8ca5f2c34b50795c6e0d9c5
thgiolito <thgiolito@gmail.com>
2021-10-29 09:23:53 +0200

    solved issues iterating over request object in happyAPI.jsx

--- src/components/happyAPI.jsx
+++ src/components/happyAPI.jsx
@@ -4,10 +4,10 @@ function RequestHappy({ tweets }) {
   return (
     tweets && (
       <div>
-        <img src={tweets.includes.media.url} alt={tweets.data.text}></img>
+        <img src={tweets.includes.media[0].url} alt={tweets.data[0].text}></img>
         <ul>
-          <li>{tweets.includes.users.name}</li>
-          <li>{tweets.data.text}</li>
+          <li>{tweets.includes.users[0].name}</li>
+          <li>{tweets.data[0].text}</li>
         </ul>
       </div>
     )
