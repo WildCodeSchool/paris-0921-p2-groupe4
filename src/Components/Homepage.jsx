0a844be7708a5f53c916a285aa4a65aea50deda7
elisacruzcas <elisa.castrillo.cruz@gmail.com>
2021-11-02 16:09:26 +0100

    Changed the design of the homepage and changed the pictures to icons

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,40 +1,43 @@
 import React from 'react';
-import Happy from '../Pictures/Happy-news.jpg';
-import National from '../Pictures/Monkey-2.jpg';
-import Tendances from '../Pictures/Tendances.jpg';
-import TED from '../Pictures/TED.jpg';
+import Happy from '../Pictures/News6.png';
+import National from '../Pictures/National8.png';
+import Tendances from '../Pictures/Tendances7.png';
+import TED from '../Pictures/TED9.png';
 import './Pictures.css';
 import './Text.css';
+import './Homepage.css';
 
 export default function Pictures() {
   return (
     <>
-      <div className="WelcomeText">
-        <p> Welcome to your relaxation App. Choose below what you want to see </p>
-      </div>
-      <div className="Container-pictures">
-        <div className="ContainerOne">
-          <div className="Pictures">
-            <a href="/Happynews">
-              <img className="Happy" src={Happy} alt="Happy-news" height="150px"></img>
-            </a>
+      <div className="Body">
+        {/* <div className="WelcomeText">
+          <p> Welcome to your relaxation App. Choose below what you want to see </p>
+        </div> */}{' '}
+        <div className="Container-pictures">
+          <div className="ContainerOne">
+            <div className="Pictures">
+              <a href="/Happynews">
+                <img className="Happy" src={Happy} alt="Happy-news" height="170px"></img>
+              </a>
+            </div>
+            <div className="Pictures">
+              <a href="/National">
+                <img className="National" src={National} alt="National-geographic" height="170px"></img>
+              </a>
+            </div>
           </div>
-          <div className="Pictures">
-            <a href="/National">
-              <img className="National" src={National} alt="National-geographic" height="170px"></img>
-            </a>
-          </div>
-        </div>
-        <div className="ContainerTwo">
-          <div className="Pictures">
-            <a href="/Tendances">
-              <img className="Tendances" src={Tendances} alt="Tendances" height="170px"></img>
-            </a>
-          </div>
-          <div className="Pictures">
-            <a href="/TedTalks">
-              <img className="TED" src={TED} alt="TED" height="170px"></img>
-            </a>
+          <div className="ContainerTwo">
+            <div className="Pictures">
+              <a href="/Tendances">
+                <img className="Tendances" src={Tendances} alt="Tendances" height="170px"></img>
+              </a>
+            </div>
+            <div className="Pictures">
+              <a href="/TedTalks">
+                <img className="TED" src={TED} alt="TED" height="170px"></img>
+              </a>
+            </div>
           </div>
         </div>
       </div>
