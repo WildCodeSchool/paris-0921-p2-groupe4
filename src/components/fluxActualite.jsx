175a91f7ba11da94e02b232cbffde865a0fbf1bb
gonzalezjc <jcobasgonzalez@gmail.com>
2021-11-04 09:29:58 +0100

    flux box with functional button

--- src/components/fluxActualite.jsx
+++ src/components/fluxActualite.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './fluxActualite.css';
 
 function FluxA() {
+  const [theButton, setButton] = React.useState(false);
+
+  function handleClickButton() {
+    setButton(!theButton);
+  }
+
   return (
     <div className="fluxbox">
       <div className="flux">
-        <p>
-          Les insectes (Insecta) sont une classe animaux invertébrés de embranchement des arthropodes et du sous-embranchement des hexapodes. Ils
-          caractérisés par un corps segmenté en trois tagmes (tête possédant des pièces buccales externes, une paire de antennes et au moins paire
-          yeux composés ; thorax pourvu de trois paires de pattes.
-        </p>
+        <div className={theButton ? 'text' : 'lesstext'}>
+          <p>
+            Les insectes (Insecta) sont une classe animaux invertébrés de embranchement des arthropodes et du sous-embranchement des hexapodes. Ils
+            caractérisés par un corps segmenté en trois tagmes (tête possédant des pièces buccales externes, une paire de antennes et au moins paire
+            yeux composés ; thorax pourvu de trois paires de pattes.
+          </p>
+        </div>
         <div>
-          <button className="fluxbutton">+</button>
+          <button className="fluxbutton" onClick={handleClickButton}>
+            &darr; &uarr;
+          </button>
         </div>
       </div>
     </div>
