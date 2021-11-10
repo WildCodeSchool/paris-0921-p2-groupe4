761366847704a3ade88f3933bc82e367a0565ccc
gonzalezjc <jcobasgonzalez@gmail.com>
2021-11-10 14:55:05 +0100

    a propos + features

---
+++ src/components/preferences.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './preferences.css';
+
+function Preferences() {
+  return (
+    <div className="preferences">
+      <h3>Preferences</h3>
+      <button className="preferencesbtn">&#8595;</button>
+    </div>
+  );
+}
+
+export default Preferences;
