9ed956d9038863e1fe572a56397d70c08157a59f
elisacruzcas <elisa.castrillo.cruz@gmail.com>
2021-11-17 14:41:42 +0100

    improved design on form display

--- src/components/Ted.jsx
+++ src/components/Ted.jsx
@@ -46,7 +46,7 @@ function Ted() {
     <div className="PageScreen">
       <div className="TextBox"> Get inspired! </div>
       <div className="TextBox">
-        <p> You&apos;re viewing : &apos;{theme}&apos; </p>
+        <p> You&apos;re viewing: &apos;{theme}&apos; </p>
         <ThemeButton handleNewTheme={handleNewTheme} handleThemeChange={handleThemeChange} newTheme={newTheme} />
         <NumberButton addTenMoreTweets={addTenMoreTweets} />
         <ImageButton handleTweetImages={handleTweetImages} />
